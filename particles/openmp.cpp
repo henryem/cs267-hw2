0aa7b5b98aa212d7ac17a5548152a644e7593dff
Henry Milner <henry@conviva.com>
2015-03-02 13:22:41 -0800

    Partial work on removing an important serial bottleneck in openmp code.

--- particles/openmp.cpp
+++ particles/openmp.cpp
@@ -62,16 +62,66 @@ int main( int argc, char **argv )
     Stats per_thread_stats[max_num_threads];
 
     // Shared across threads.
+#ifdef PARALLEL_GRID
+    std::unique_ptr<Grid> g0(new Grid(size, num_grid_squares_per_side));
+    std::unique_ptr<Grid> g1(new Grid(size, num_grid_squares_per_side));
+#else
     std::unique_ptr<Grid> g(new Grid(size, num_grid_squares_per_side));
+#endif
 
     #pragma omp parallel
     {
     numthreads = omp_get_num_threads();
     for (int step = 0; step < 1000; step++) {
-      //FIXME: This is serial!  Need to make a Grid::add_all() that is
-      // thread-safe.
+      //TODO: Does this need to be declared private?
+      int thread_idx;
+
+      //FIXME: If this is the first step, initialize the grid without
+      // respecting cache locality here.  Since we cannot use the existing
+      // grid, we have to just divide the particles arbitrarily.  This
+      // means that the subsequent code for simulating forces and movement
+      // will have almost no cache locality on the first iteration: Each thread
+      // has picked up an arbitrary subset of the particles to insert into the
+      // grid, and then the threads are responsible for simulating a different,
+      // mostly-disjoint subset of the particles.  On subsequent iterations,
+      // only the particles that have moved will cause cache misses, so we
+      // should have much better locality.  If we want to really optimize,
+      // it may be worth rethinking how we store particles and communicate among
+      // threads.  But at that point we might as well write distributed-memory
+      // code.
+
+      // Here we are building the grid that maps locations to sets of
+      // particles.  This step does O(n) work, so it is a bottleneck if done
+      // serially.  For performance comparisons, we have two versions of the
+      // grid-formation code.  The second simply forms the grid serially, in a
+      // single arbitrary thread.  The first is parallel and attempts
+      // some cache locality.  Each thread is responsible for re-inserting
+      // the grid elements that previously lay in its subgrid.
+      //NOTE: We could instead re-insert each particle right after moving it.
+      // This would be faster, but it would require us to think about
+      // simultaneous parallel delete and add, while the current scheme needs
+      // only support parallel add.  (Deleting the entire grid at once is an
+      // O(1) operation, so we can do it in one thread with a barrier.)
+#ifdef PARALLEL_GRID
+      //FIXME: Explain.  We need the old Grid around to find the particles in
+      // it.
+      #pragma omp single
+      if (step % 2 == 0) {
+        g1.reset(new Grid(size, num_grid_squares_per_side));
+      } else {
+        g0.reset(new Grid(size, num_grid_squares_per_side));
+      }
+
+      thread_idx = omp_get_thread_num();
+      //FIXME: Breaks abstraction.  The Grid should divide itself up and give
+      // us an iterator over particle_t& for each thread.
+      std::unique_ptr<SimpleIterator<int> > grid_squares_for_thread = get_squares_for_thread(thread_idx);
+
+      // Now insert each guy into the new grid
+#else
       #pragma omp single
       g.reset(new Grid(size, num_grid_squares_per_side, *particles));
+#endif
 
       //TODO: Could improve data locality by blocking according to the block
       // structure of the grid.  That would require keeping track, dynamically,
@@ -80,7 +130,7 @@ int main( int argc, char **argv )
       // code) to threads improves things further.
       #pragma omp for
       for (int i = 0; i < n; i++) {
-        int thread_idx = omp_get_thread_num();
+        thread_idx = omp_get_thread_num();
         particle_t& p = (*particles)[i];
         p.ax = p.ay = 0;
         std::unique_ptr<SimpleIterator<particle_t&> > neighbors = (*g).neighbor_iterator(p);
