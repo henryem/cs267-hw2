71774c4c7858cf3b0255ad00b23c19d41f8ee9f5
Henry Milner <henry@conviva.com>
2015-03-12 12:59:36 -0700

    Add serial-naive build.

---
+++ particles/serial-naive.cpp
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <vector>
+
+#include "common.h"
+
+//
+//  benchmarking program
+//
+int main( int argc, char **argv )
+{
+
+    if( find_option( argc, argv, "-h" ) >= 0 )
+    {
+        printf( "Options:\n" );
+        printf( "-h to see this help\n" );
+        printf( "-n <int> to set the number of particles\n" );
+        printf( "-o <filename> to specify the output file name\n" );
+        printf( "-s <filename> to specify a summary file name\n" );
+        printf( "-no turns off all correctness checks and particle output\n");
+        return 0;
+    }
+    
+    int n = read_int( argc, argv, "-n", 1000 );
+
+    char *savename = read_string( argc, argv, "-o", NULL );
+    char *sumname = read_string( argc, argv, "-s", NULL );
+    
+    FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
+    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;
+
+
+    Stats overall_stats;
+    const double size = set_size(n);
+    std::unique_ptr<std::vector<particle_t>> particles_v = init_particles(n);
+    particle_t* particles = particles_v->data();
+    
+    //
+    //  simulate a number of time steps
+    //
+    double simulation_time = read_timer( );
+	
+    for( int step = 0; step < NSTEPS; step++ )
+    {
+        //
+        //  compute forces
+        //
+        for( int i = 0; i < n; i++ )
+        {
+            particles[i].ax = particles[i].ay = 0;
+            for (int j = 0; j < n; j++ )
+				apply_force( particles[i], particles[j], overall_stats);
+        }
+ 
+        //
+        //  move particles
+        //
+        for( int i = 0; i < n; i++ ) 
+            move( particles[i] );		
+
+        if( find_option( argc, argv, "-no" ) == -1 )
+        {
+          //
+          //  save if necessary
+          //
+          if( fsave && (step%SAVEFREQ) == 0 )
+              save( fsave, n, particles );
+        }
+    }
+    simulation_time = read_timer( ) - simulation_time;
+    
+    printf( "n = %d, simulation time = %g seconds", n, simulation_time);
+
+    if( find_option( argc, argv, "-no" ) == -1 )
+    {
+    // 
+    //  -the minimum distance absmin between 2 particles during the run of the simulation
+    //  -A Correct simulation will have particles stay at greater than 0.4 (of cutoff) with typical values between .7-.8
+    //  -A simulation were particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
+    //
+    //  -The average distance absavg is ~.95 when most particles are interacting correctly and ~.66 when no particles are interacting
+    //
+    printf( ", absmin = %lf, absavg = %lf", overall_stats.min, overall_stats.avg);
+    if (overall_stats.min < 0.4) printf ("\nThe minimum distance is below 0.4 meaning that some particle is not interacting");
+    if (overall_stats.avg < 0.8) printf ("\nThe average distance is below 0.8 meaning that most particles are not interacting");
+    }
+    printf("\n");     
+
+    //
+    // Printing summary data
+    //
+    if( fsum) 
+        fprintf(fsum,"%d %g\n",n,simulation_time);
+ 
+    //
+    // Clearing space
+    //
+    if( fsum )
+        fclose( fsum );
+    if( fsave )
+        fclose( fsave );
+    
+    return 0;
+}
